1fe0b929f09f5af18055f5bbe6ff52211bd45986
shubh ujala <shubhujala.code@gmail.com>
2025-04-26 23:43:35 +0530

    updated

--- 01_fs_module/file.js
+++ 01_fs_module/file.js
@@ -3,5 +3,9 @@ const fs = require('fs');
 // we can see we haven't used the ./fs because we are not importing the things from the current directory! 
 
 // How to create a file ✅ (writeFileSync)
-// fs.writeFileSync("01_sample.txt","hi i have created using fs module");
+fs.writeFileSync("./01_sample.txt","hi i have created using fs module");//creating file inside the same directory !
 //whenever we will run this file using node a file named "01_sample.txt" is going to be created which is having the content "hi i have created using fs module";
+
+fs.writeFileSync("../01_sample.txt","hi i have created using fs module"); //creating file outside the directory !
+
+
