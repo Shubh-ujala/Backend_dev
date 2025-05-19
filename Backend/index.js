dbe8a92ee68239518fd6f1ba2419f4d0e5cbe92c
shubh ujala <shubhujala.code@gmail.com>
2025-05-19 15:38:38 +0530

    basic node.js studied

--- Backend/index.js
+++ Backend/index.js
@@ -28,3 +28,5 @@ const path = require("path");
 console.log(__dirname); // prints the directory in which we are working!
 console.log(path.join(__dirname,"index.js")); // it will joins the index.js to the path!
 
+// the main thing that we have to keep in mind is we don't need to push the node_modules to our github as it is very heavy file by heavy file i mean it is having a bunch of dependencies that's why whenever we are pushing our code to the github we just have to delete the node_modules folder and then commit the changes , if we want the node_module folder back then we just have to run `npm install` it will automatically read the dependencies fron package.json file and install all the dependencies that are required !
+
